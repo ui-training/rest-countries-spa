40915d1964279d6318d163a2420bd7f805bc0bcb
Prakash Kumar <mail@prakash.io>
2018-01-25 07:41:32 +0530

    Created a component and moved all the country list information into that.

--- app.js
+++ app.js
@@ -1,29 +1,2 @@
 var app = angular.module('restCountriesApp', []);
 
-app.controller('TestController', ['$scope', function ($scope) {
-    $scope.message = 'Loaded the test controller';
-}]);
-
-app.controller('CountryList', ['$http', function ($http) {
-
-    var controller = this;
-    controller.countries = [];
-
-    var promise = $http.get('https://restcountries.eu/rest/v2/all?fields=name;capital;population;')
-
-    promise
-
-        .then(function(response) {
-
-            controller.countries = response.data;
-            console.log('Countries', response.data);
-        })
-
-        .catch(function(err) {
-
-            controller.error = 'Error while fetching the countries'
-            console.log('Error', err);
-        })
-
-    controller.status = 'Loaded';
-}]);
